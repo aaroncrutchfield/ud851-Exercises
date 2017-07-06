be6cc50a7494eb8522919eb9434bf4d0a18a3c52
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2017-07-06 09:04:20 -0400

    Created new intent

--- Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -62,15 +62,20 @@ public class MainActivity extends AppCompatActivity {
                 Context context = MainActivity.this;
 
                 // TODO (1) Store ChildActivity.class in a Class object called destinationActivity
+                Activity childActivity = ChildActivity.class;
 
                 // TODO (2) Create an Intent to start ChildActivity
+                Intent intent = new Intent(this, childActivity);
 
                 // TODO (3) Replace the Toast with code to start ChildActivity
-                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                intent.startActivity(intent);
+                
+                //String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
+                //Toast.makeText(context, message, Toast.LENGTH_LONG).show();
 
             }
         });
     }
 }
 
+)
